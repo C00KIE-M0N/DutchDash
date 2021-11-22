d6a1e7b1d5366936e593da93135be08ef180b0a1
jacobvddungen <jacobkubus@gmail.com>
2021-11-22 11:47:53 +0100

    player script

--- Assets/Scripts/Playercontroller.cs
+++ Assets/Scripts/Playercontroller.cs
@@ -6,6 +6,9 @@ public class Playercontroller : MonoBehaviour
 {
     [SerializeField]GameObject Player;
 
+    [SerializeField] float xMin = -2, xMax = 2;
+    [SerializeField] float journyTime = 1f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,14 +19,17 @@ public class Playercontroller : MonoBehaviour
     void Update()
     {
 
-        if (Input.GetKeyDown(KeyCode.D))
+        if (Input.GetKey(KeyCode.D))
         {
             Player.transform.position += new Vector3(1, 0, 0);
         }
 
-        if (Input.GetKeyDown(KeyCode.A))
+        if (Input.GetKey(KeyCode.A))
         {
             Player.transform.position += new Vector3(-1, 0, 0);
         }
+
+        Vector3 newpos = new Vector3(Mathf.Clamp(Player.transform.position.x, xMin, xMax), Player.transform.position.y, Player.transform.position.z);
+        Player.transform.position = Vector3.Slerp(Player.transform.position, newpos, journyTime);
     }
 }
