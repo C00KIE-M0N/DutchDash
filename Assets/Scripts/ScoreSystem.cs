e377c4415e7b816efa2fab75ac459c90cad13050
C00KIE-M0N <45165591+C00KIE-M0N@users.noreply.github.com>
2021-11-23 14:01:35 +0100

    Score system code
    added Logs to git ignore

--- Assets/Scripts/ScoreSystem.cs
+++ Assets/Scripts/ScoreSystem.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class ScoreSystem : MonoBehaviour
 {
+    public float ScoreMod;
+
+    [SerializeField] private float m_totalScore;
+
     private Vector3 m_oldPosisition;
     private float m_totalDistance;
 
-    void Start()
+    private void Start()
     {
         m_oldPosisition = transform.position;
     }
 
-    void Update()
+    private void Update()
     {
         CalculateDistance();
     }
@@ -24,5 +28,7 @@ public class ScoreSystem : MonoBehaviour
 
         m_totalDistance += _distancetravelledthisframe;
         m_oldPosisition = transform.position;
+
+        m_totalScore = m_totalDistance * ScoreMod;
     }
 }
