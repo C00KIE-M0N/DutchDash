5c5caafda1cc4334a25e7c1bb16fd17955af47a0
GirlGamerNL <57950826+GirlGamerNL@users.noreply.github.com>
2021-11-24 15:37:44 +0100

    Game Fixed

--- Assets/Code/death.cs
+++ Assets/Code/death.cs
@@ -5,11 +5,20 @@ using UnityEngine.SceneManagement;
 
 public class death : MonoBehaviour
 {
+    public GameObject DeathPanel;
+
     private void OnTriggerEnter(Collider Other)
     {
         if (Other.CompareTag("Player"))
         {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            Time.timeScale = 0f;
+            DeathPanel.SetActive(true);
         }
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        Time.timeScale = 1f;
     }
 }
