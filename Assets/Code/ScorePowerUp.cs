dabf0e85338a5cb43158ca9d5fc84de22b88aec3
GirlGamerNL <57950826+GirlGamerNL@users.noreply.github.com>
2021-11-29 09:58:21 +0100

    Score2X

---
+++ Assets/Code/ScorePowerUp.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScorePowerUp : MonoBehaviour
+{
+
+    public bool powerup;
+    public float timer = 10;
+
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "ScoreUp")
+        {
+            powerup = true;
+        }
+    }
+
+    private void Update()
+    {
+        if (powerup)
+        {   
+            //score x2
+            ScoreSystem.instance.ScoreMod = 2;
+            timer -= 1 * Time.deltaTime;
+        }
+
+        if (timer <= 0)
+        {
+            powerup = false;
+            ScoreSystem.instance.ScoreMod = 1;
+            timer = 10;
+        }
+    }
+}
