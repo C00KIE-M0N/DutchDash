756984e8999371d4a88ea66c7009699c05671b24
GirlGamerNL <57950826+GirlGamerNL@users.noreply.github.com>
2021-12-08 14:19:31 +0100

    Startscreen werkt

--- Assets/Scripts/UI.cs
+++ Assets/Scripts/UI.cs
@@ -1,20 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class UI : MonoBehaviour
 {
-    public GameObject StartPanel;
-
-    private void Start()
-    {
-        Time.timeScale = 0f;
-        StartPanel.SetActive(true);
-    }
-
     public void StartButton()
     {
-        StartPanel.SetActive(false);
-        Time.timeScale = 1f;
+        SceneManager.LoadScene("CompleteGame");
+    }
+
+    public void QuitButton()
+    {
+        Application.Quit();
     }
 }
